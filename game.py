ba315ddb501a9d644c049409df77e9ed2ee70bac
lizcoding <elizarn96@gmail.com>
2021-11-30 11:28:32 -0800

    Display player greeting

--- game.py
+++ game.py
@@ -1,4 +1,33 @@
 """A number-guessing game."""
 
 # Put your code here
-print("hi")
+
+print("Howdy, what's your name?")
+name = input("(type in your name) ")
+
+
+"""
+$ python3 game.py
+Howdy, what's your name?
+(type in your name) Jessica
+Jessica, I'm thinking of a number between 1 and 100.
+Try to guess my number.
+Your guess? 50
+Your guess is too low, try again.
+Your guess? 80
+Your guess is too high, try again.
+Your guess? 60
+Your guess is too low, try again.
+Your guess? 70
+Your guess is too high, try again.
+Your guess? 63
+Your guess is too low, try again.
+Your guess? 64
+Your guess is too low, try again.
+Your guess? 67
+Your guess is too low, try again.
+Your guess? 69
+Your guess is too high, try again.
+Your guess? 68
+Well done, Jessica! You found my number in 9 tries!
+"""
