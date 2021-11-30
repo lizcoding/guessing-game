5d551bc834c481374b8723dad0521d98d9bdb1a5
lizcoding <elizarn96@gmail.com>
2021-11-30 11:38:58 -0800

    Computer message

--- game.py
+++ game.py
@@ -1,9 +1,14 @@
 """A number-guessing game."""
 
 # Put your code here
+from random import randint
+
 
 print("Howdy, what's your name?")
 name = input("(type in your name) ")
+
+print(f"{name}, I'm thinking of a number between 1 and 100.")
+print("Try to guess my number.")
 
 
 """
