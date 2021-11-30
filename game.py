565cbe05a4a2a0cb01d114cc88203f943e3587d6
lizcoding <elizarn96@gmail.com>
2021-11-30 14:48:15 -0800

    Complete game logic

--- game.py
+++ game.py
@@ -15,42 +15,18 @@ count = 0
 
 while True:
     guess = input("Your guess? ")
+    if not guess.isdigit():
+        print("Please enter digits only.")
+        continue
+    guess = int(guess)
+    if guess > 100 or guess < 1:
+        print("Please use a number between 1 and 100.")
+        continue
     count += 1
     if guess == correct_num:
-        pass
+        print(f"Well done, {name}! You found my number in {count} tries!")
+        break
+    elif guess < correct_num:
+        print("You guessed too low! Try again.")
     else:
-        pass
-
-"""
-get user's guess
-check if guess is correct
-if guess is correct:
-    break
-else:
-    tell them if it is too low or too high
-"""
-
-
-"""
-$ python3 game.py
-the loop part:
-
-Your guess? 50
-Your guess is too low, try again.
-Your guess? 80
-Your guess is too high, try again.
-Your guess? 60
-Your guess is too low, try again.
-Your guess? 70
-Your guess is too high, try again.
-Your guess? 63
-Your guess is too low, try again.
-Your guess? 64
-Your guess is too low, try again.
-Your guess? 67
-Your guess is too low, try again.
-Your guess? 69
-Your guess is too high, try again.
-Your guess? 68
-Well done, Jessica! You found my number in 9 tries!
-"""
+        print("You guessed too high! Try again.")
